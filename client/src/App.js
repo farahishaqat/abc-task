2114653797e2da780c5d34767d135d392f6178b9
farahishaqat <ishaqatfarah@gmail.com>
2021-04-23 00:45:38 +0300

    get all complaints oon front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,78 @@
-import React, { useState } from "react";
+import React ,{useEffect,useState}from "react";
 import "./App.css"
+import axios from 'axios';
 
 function App() {
+//post data to the database
+  const[complaintId ,setcomplaintId]= useState('');
+  const[complaintDescription, setComplaintDescription]=useState('');
+  const[user_Id, setuser_Id]=useState('');
+  const[complaintStatus, setcomplaintStatus]=useState('');
+
+  const submitComplaint=()=>{
+    axios.post('http://localhost:3001/api/insert',{
+      complaintId:complaintId,
+      complaintDescription:complaintDescription,
+      user_Id:user_Id,
+      complaintStatus:complaintStatus
+
+    }).then(()=>{alert("complaint added successfully")})
+
+  }
+
+
+  //display complaints in front end
+
+  const [complaintList,setcomplaintList]=useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:3001/api/get').then((response)=>{
+      // console.log(response.data)
+      setcomplaintList(response.data)
+    });
+  },[]);
+  //
 
   return (
     
       <div className="App"> <h4>
         CRUD
       </h4>
-      <div className="form">
-        
-        <label> name</label>
-        <input type="text" name="name"/>
-        <label> complaint</label>
-        <input type="text" name="complaint"/>
+<div className="form">
+<label>complaintId</label>
+<input 
+type="text" 
+name="name" 
+onChange={(e)=>{setcomplaintId(e.target.value);} }/>
 
-        <button>Submit</button>
-      </div>
-        
+<label>complaint Description</label>
+<input type="text"
+name="complaintDescription"
+onChange={(e)=>{setComplaintDescription(e.target.value);} }/>
+
+
+<label>User Id</label>
+<input 
+type="text" 
+name="name" 
+onChange={(e)=>{setuser_Id(e.target.value);} }/>
+
+<label>complaintStatus</label>
+<input 
+type="text" 
+name="name" 
+onChange={(e)=>{setcomplaintStatus(e.target.value);} }/>
+
+<button onClick={submitComplaint}> Submit</button>
+
+  {complaintList.map((val)=>{
+    return  <p> complaintId : {val.complaintId}
+    complaintDescription : {val.complaintDescription}
+    user_Id : {val.user_Id}
+    complaintStatus : {val.complaintStatus}</p>
+  })}
+  </div>
+      
+    
       </div>
 
   );
--- server/index.js
+++ server/index.js
@@ -1,15 +1,10 @@
 const express =require('express');
+const bodyParser=require('body-parser')
+const cors= require('cors')
+
 const app = express();
 
-//-------------
 const mysql =require('mysql');
-
-/*from clear DB
-// username b122385535251b
-// password eea18bb3
-// host us-cdbr-east-03.cleardb.com
-// database heroku_5bcb5d7d38aaa3e
-// reconnect=true*/
 
 const db=mysql.createPool({
     host:'us-cdbr-east-03.cleardb.com',
@@ -18,18 +13,43 @@ const db=mysql.createPool({
     database:'heroku_5bcb5d7d38aaa3e'
 })
 
+// app.get("/",(req,res)=>{
 
+//     const sqlInsert ="INSERT INTO complaints (complaintId,complaintDescription,user_Id,complaintStatus) VALUES('11','so mad about the customer service','234','resolved')"
+// db.query(sqlInsert,(err,result)=>{
+//     res.send("hello farah")
+// })
+// });
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
-
-
-//-------------
-app.get("/",(req,res)=>{
-
-    const sqlInsert ="INSERT INTO complaints (complaintId,complaintDescription,user_Id,complaintStatus) VALUES('11','so mad about the customer service','234','resolved')"
-db.query(sqlInsert,(err,result)=>{
-    res.send("hello farah")
-})
+//post complaints to the database
+app.post("/api/insert",(req,res)=>{
+    
+    const complaintId=req.body.complaintId
+    const complaintDescription=req.body.complaintDescription
+    const user_Id=req.body.user_Id
+    const complaintStatus=req.body.complaintStatus
+    
+    const sqlInsert="INSERT INTO complaints (complaintId,complaintDescription,user_Id,complaintStatus) VALUES(?,?,?,?)"
+    db.query(sqlInsert,[complaintId,complaintDescription,user_Id,complaintStatus],(err,result)=>{
+// console.log(result)
+    })
 });
+
+
+//get all complaints from Database
+
+app.get("/api/get",(req,res)=>{
+   
+    const sqlSelect ="SELECT * FROM complaints";
+    db.query(sqlSelect,(err,result)=>{
+// console.log(result)
+res.send(result)
+    })
+})
+
 
 
 app.listen(3001,()=>{
